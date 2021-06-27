92fc2e6d8e5e2e803685408e66be3731d8d0d237
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-06-27 18:02:14 +0900

    ユーザー管理機能

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "ユーザー新規登録" do
+    it "emailが空だと登録できない" do
+    end
+    it "nicknameが空だと登録できない" do
+    end
+    it "passwordが空だと登録できない" do
+    end
+    it "last_nameが空だと登録できない" do
+    end
+    it "first_nameが空だと登録できない" do
+    end
+    it "ruby_last_nameが空だと登録できない" do
+    end
+    it "ruby_first_nameが空だと登録できない" do
+    end
+    it "birthdayが空だと登録できない" do
+    end
+  end
+end
