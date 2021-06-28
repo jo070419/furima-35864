cb39bd4ed481d5d56fc5458baf744f0e822c5cc6
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-06-28 14:18:50 +0900

    コミット漏れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :ruby_last_name, presence: true
-  validates :ruby_first_name, presence: true
-  validates :birthday, presence: true
-  
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}}
+    validates :ruby_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :ruby_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday
+
 end
