94526818f85943bd4e1c5ae3ece6531f8a976105
jo070419 <baseball.greeeen.0704@docomo.ne.jp>
2021-07-07 16:50:01 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @item = Item.order(id: "ASC")
+    @item = Item.order(id: "DESC")
   end
 
   def new
